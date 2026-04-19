1489bcd59629d42f21522413427d06f9017aa130
Lxrcade <167519963+Lxrcade@users.noreply.github.com>
2026-04-19 23:32:25 +0800

    feat: implement service worker caching and initialize main application layout and roster tab components

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0bk5no7k4e"
+    "revision": "0.50tjd7r3pro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
